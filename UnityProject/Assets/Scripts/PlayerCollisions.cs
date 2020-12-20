66dd0f9220898983ccb29545809ca851bd1d3763
Erik Nyquist <eknyquist@gmail.com>
2020-12-19 22:04:21 -0800

    Turn pickup items into triggers
    This way, zombies can walk right through them, but we will still get a
    collision when the player collides with one

--- UnityProject/Assets/Scripts/PlayerCollisions.cs
+++ UnityProject/Assets/Scripts/PlayerCollisions.cs
@@ -18,7 +18,7 @@ public class PlayerCollisions : MonoBehaviour
     public AudioClip ammoPickupSound;
     public AudioClip healthPickupSound;
     public AudioClip playerHitSound;
-    
+
     AudioSource audioSource;
     GameObject player;
     PlayerHUD playerHUD;
@@ -26,36 +26,36 @@ public class PlayerCollisions : MonoBehaviour
     SpriteRenderer rend;
     int health;
     bool coolingOff = false;
-    
+
     /* Tracks the number of zombies touching us-- we increment in OnCollisionEnter
      * and decrement in OnCollisionExit */
     int touching = 0;
-    
+
     void Start()
     {
         audioSource = GetComponent<AudioSource>();
         player = GameObject.FindGameObjectWithTag("Player");
         GameObject hud = GameObject.FindGameObjectWithTag("PlayerHUD");
         playerHUD = hud.GetComponent<PlayerHUD>();
-        
+
         health = maxHealth;
         rend = gameObject.GetComponent<SpriteRenderer>();
     }
-    
+
     void EnterCoolOff()
     {
         coolingOff = true;
         InvokeRepeating("toggleRenderer", 0, coolOffFlashDelay);
     }
-    
+
     void ExitCoolOff()
     {
         CancelInvoke("toggleRenderer");
         coolingOff = false;
-        
+
         // make sure player rendering is enabled
         rend.enabled = true;
-        
+
         /* Finally, check if there are currently zombies touching us, in which
          * case we'll trigger another zombie hit immediately */
          if (touching > 0)
@@ -63,18 +63,18 @@ public class PlayerCollisions : MonoBehaviour
             PlayerHitByZombie();
          }
     }
-    
+
     void toggleRenderer()
     {
         rend.enabled = !rend.enabled;
     }
-  
+
     void Death()
     {
         Scene scene = SceneManager.GetActiveScene();
-        SceneManager.LoadScene(scene.name);   
+        SceneManager.LoadScene(scene.name);
     }
-    
+
     void PlayerHitByZombie()
     {
         // Decrement player's health
@@ -100,48 +100,55 @@ public class PlayerCollisions : MonoBehaviour
     {
         GameObject collided = collision.gameObject;
         Zombie zombie = collided.GetComponent<Zombie>();
-        
+
         if (zombie != null)
         {
             // Exiting collision with a zombie
             touching -= 1;
         }
     }
-    
-    void OnCollisionEnter2D(Collision2D collision)
+
+    void OnTriggerEnter2D(Collider2D other)
     {
         Pickup pickup;
+
+        switch (other.gameObject.tag)
+        {
+            case "Ammo":
+                audioSource.PlayOneShot(ammoPickupSound, 0.5f);
+                playerHUD.ammoCounter.Reload();
+                pickup = other.gameObject.GetComponent<Pickup>();
+                pickup.PickedUp();
+                break;
+
+            case "Health":
+                audioSource.PlayOneShot(healthPickupSound, 0.5f);
+                pickup = other.gameObject.GetComponent<Pickup>();
+                pickup.PickedUp();
+                SetMaxHealth();
+                break;
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
         GameObject collided = collision.gameObject;
-        
+
         switch (collided.tag)
         {
             case "Zombie":
                 Zombie zombie = collided.GetComponent<Zombie>();
                 touching += 1;
-            
+
                 if (!coolingOff)
                 {
                     // Finished cooling off from last zombie hit
                     PlayerHitByZombie();
                 }
                 break;
-
-            case "Ammo":
-                audioSource.PlayOneShot(ammoPickupSound, 0.5f);
-                playerHUD.ammoCounter.Reload();
-                pickup = collided.GetComponent<Pickup>();
-                pickup.PickedUp();
-                break;
-
-            case "Health":
-                audioSource.PlayOneShot(healthPickupSound, 0.5f);
-                pickup = collided.GetComponent<Pickup>();
-                pickup.PickedUp();
-                SetMaxHealth();
-                break;
-        }    
+        }
     }
-    
+
     public void SetMaxHealth()
     {
         health = maxHealth;
